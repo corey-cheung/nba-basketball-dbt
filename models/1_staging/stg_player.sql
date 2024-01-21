f6c765fcbc0f0a96ee89f277bb1f5203baadf437
Corey Cheung <coreycheung@outlook.com>
2024-01-21 22:22:57 +1000

    fix json extract operator to handle strings correctly

--- models/1_staging/stg_player.sql
+++ models/1_staging/stg_player.sql
@@ -6,14 +6,14 @@ WITH player AS (
 final AS (
 
     SELECT
-        (_airbyte_data -> 'player_id')::INTEGER AS player_id,
-        (_airbyte_data -> 'position')::TEXT AS position,
-        (_airbyte_data -> 'team_id')::INTEGER AS team_id,
-        (_airbyte_data -> 'first_name')::TEXT AS first_name,
-        (_airbyte_data -> 'last_name')::TEXT AS last_name,
-        (_airbyte_data -> 'height_feet')::INTEGER AS height_feet,
-        (_airbyte_data -> 'height_inches')::INTEGER AS height_inches,
-        (_airbyte_data -> 'weight_pounds')::INTEGER AS weight_pounds,
+        (_airbyte_data ->> 'player_id')::INTEGER AS player_id,
+        (_airbyte_data ->> 'position')::TEXT AS position,
+        (_airbyte_data ->> 'team_id')::INTEGER AS team_id,
+        (_airbyte_data ->> 'first_name')::TEXT AS first_name,
+        (_airbyte_data ->> 'last_name')::TEXT AS last_name,
+        (_airbyte_data ->> 'height_feet')::INTEGER AS height_feet,
+        (_airbyte_data ->> 'height_inches')::INTEGER AS height_inches,
+        (_airbyte_data ->> 'weight_pounds')::INTEGER AS weight_pounds,
         {{ airbyte_raw_fields() }}
     FROM player
 )
